8c992a8019ead5e727074f35274554d906ae8dda
Leandro <leandro.peralta@live.com.ar>
2014-04-06 06:09:46 +1200

    New about page and overflow menu option.

--- app/src/main/java/com/androideas/quakesnz/app/ui/MainActivity.java
+++ app/src/main/java/com/androideas/quakesnz/app/ui/MainActivity.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.androideas.quakesnz.app.R;
@@ -161,4 +163,23 @@ public class MainActivity extends FragmentActivity {
         intent.putExtra(DetailActivity.EXTRA_FEATURE, feature);
         startActivity(intent);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_about) {
+            startActivity(new Intent(this, AboutActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
