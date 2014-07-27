748d854087cd3ffac4c9c39ca0c61043dd69a8cb
Leandro Martín Peralta <leandro.peralta@powershop.co.nz>
2014-07-28 09:02:01 +1200

    Adjustments to about and info screens.

--- app/src/main/java/com/androideas/quakesnz/app/ui/AboutActivity.java
+++ app/src/main/java/com/androideas/quakesnz/app/ui/AboutActivity.java
@@ -1,9 +1,10 @@
 package com.androideas.quakesnz.app.ui;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.text.util.Linkify;
+import android.widget.TextView;
+
 import com.androideas.quakesnz.app.R;
 
 public class AboutActivity extends ActionBarActivity {
@@ -12,5 +13,7 @@ public class AboutActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        Linkify.addLinks((TextView) findViewById(R.id.about_body), Linkify.ALL);
+        Linkify.addLinks((TextView) findViewById(R.id.acknowledgements_body), Linkify.ALL);
     }
 }
