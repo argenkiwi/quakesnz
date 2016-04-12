695cdca4abe484d7e52c97f433720694fc878d82
Leandro Martín Peralta <leandro.peralta@powershop.co.nz>
2016-04-13 09:12:19 +1200

    Proguard related changes.

--- app/src/main/java/nz/co/codebros/quakesnz/model/FeatureCollection.java
+++ app/src/main/java/nz/co/codebros/quakesnz/model/FeatureCollection.java
@@ -1,14 +1,13 @@
 package nz.co.codebros.quakesnz.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class FeatureCollection {
 
+    @SerializedName("features")
     private Feature[] features;
 
     public Feature[] getFeatures() {
         return features;
-    }
-
-    public void setFeatures(Feature[] features) {
-        this.features = features;
     }
 }
