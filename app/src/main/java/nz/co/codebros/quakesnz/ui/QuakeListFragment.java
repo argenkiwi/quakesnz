e5998f4b0d1a785e443253c2f1dbb9f39bb7dd2b
Leandro Martín Peralta <leandro.peralta@powershop.co.nz>
2016-07-04 09:16:50 +1200

    Fixed formatting.

--- app/src/main/java/nz/co/codebros/quakesnz/ui/QuakeListFragment.java
+++ app/src/main/java/nz/co/codebros/quakesnz/ui/QuakeListFragment.java
@@ -2,7 +2,6 @@ package nz.co.codebros.quakesnz.ui;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
