76cb3bd992afc92857d8a8419bdfee8d66c7000e
Leandro <gringoperalta@gmail.com>
2017-10-26 14:55:21 +1300

    Simplify.

--- app/src/main/java/nz/co/codebros/quakesnz/QuakesUtils.kt
+++ app/src/main/java/nz/co/codebros/quakesnz/QuakesUtils.kt
@@ -10,25 +10,21 @@ import nz.co.codebros.quakesnz.R
  */
 object QuakesUtils {
 
-    fun getColor(context: Context, mmi: Int): Int {
-        when (mmi) {
-            12, 11, 10, 9, 8, 7 -> return ContextCompat.getColor(context, R.color.severe)
-            6 -> return ContextCompat.getColor(context, R.color.strong)
-            5 -> return ContextCompat.getColor(context, R.color.moderate)
-            4 -> return ContextCompat.getColor(context, R.color.light)
-            3 -> return ContextCompat.getColor(context, R.color.weak)
-            else -> return ContextCompat.getColor(context, R.color.unnoticeable)
-        }
+    fun getColor(context: Context, mmi: Int): Int = when (mmi) {
+        12, 11, 10, 9, 8, 7 -> ContextCompat.getColor(context, R.color.severe)
+        6 -> ContextCompat.getColor(context, R.color.strong)
+        5 -> ContextCompat.getColor(context, R.color.moderate)
+        4 -> ContextCompat.getColor(context, R.color.light)
+        3 -> ContextCompat.getColor(context, R.color.weak)
+        else -> ContextCompat.getColor(context, R.color.unnoticeable)
     }
 
-    fun getIntensity(context: Context, mmi: Int): String {
-        when (mmi) {
-            12, 11, 10, 9, 8, 7 -> return context.getString(R.string.severe)
-            6 -> return context.getString(R.string.strong)
-            5 -> return context.getString(R.string.moderate)
-            4 -> return context.getString(R.string.light)
-            3 -> return context.getString(R.string.weak)
-            else -> return context.getString(R.string.unnoticeable)
-        }
+    fun getIntensity(context: Context, mmi: Int): String = when (mmi) {
+        12, 11, 10, 9, 8, 7 -> context.getString(R.string.severe)
+        6 -> context.getString(R.string.strong)
+        5 -> context.getString(R.string.moderate)
+        4 -> context.getString(R.string.light)
+        3 -> context.getString(R.string.weak)
+        else -> context.getString(R.string.unnoticeable)
     }
 }
