be81425b4e482d5e4fc71dae1fdc37f71fe9cfe6
Leandro <gringoperalta@gmail.com>
2017-11-20 13:30:00 +1300

    Let interactor expose completable.

--- app/src/main/java/nz/co/codebros/quakesnz/interactor/LoadFeatureInteractor.kt
+++ app/src/main/java/nz/co/codebros/quakesnz/interactor/LoadFeatureInteractor.kt
@@ -1,12 +1,10 @@
 package nz.co.codebros.quakesnz.interactor
 
-import io.reactivex.disposables.Disposable
-import io.reactivex.functions.Action
-import io.reactivex.functions.Consumer
+import io.reactivex.Completable
 
 /**
  * Created by leandro on 10/06/17.
  */
 interface LoadFeatureInteractor {
-    fun execute(publicId: String, onComplete: Action, onError: Consumer<Throwable>): Disposable
+    fun execute(publicId: String): Completable
 }
--- app/src/main/java/nz/co/codebros/quakesnz/interactor/LoadFeatureInteractorImpl.kt
+++ app/src/main/java/nz/co/codebros/quakesnz/interactor/LoadFeatureInteractorImpl.kt
@@ -1,9 +1,6 @@
 package nz.co.codebros.quakesnz.interactor
 
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
-import io.reactivex.functions.Action
-import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
 import nz.co.codebros.quakesnz.core.GeonetService
 import nz.co.codebros.quakesnz.repository.FeatureRepository
@@ -16,15 +13,10 @@ class LoadFeatureInteractorImpl(
         private val service: GeonetService,
         private val repository: FeatureRepository
 ) : LoadFeatureInteractor {
-    override fun execute(
-            publicId: String,
-            onComplete: Action,
-            onError: Consumer<Throwable>
-    ): Disposable = service.getQuake(publicId)
+    override fun execute(publicId: String) = service.getQuake(publicId)
             .map { (features) -> features[0] }
             .doOnSuccess { repository.observer.onNext(it) }
             .toCompletable()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(onComplete, onError)
 }
