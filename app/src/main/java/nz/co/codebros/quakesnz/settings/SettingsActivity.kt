5a6b48c32b2f9015d0481abc715d5731ec006fb0
Leandro <gringoperalta@gmail.com>
2017-11-23 12:16:38 +1300

    Fix settings crash.

--- app/src/main/java/nz/co/codebros/quakesnz/settings/SettingsActivity.kt
+++ app/src/main/java/nz/co/codebros/quakesnz/settings/SettingsActivity.kt
@@ -2,14 +2,12 @@ package nz.co.codebros.quakesnz.settings
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import dagger.android.support.DaggerAppCompatActivity
-
 import nz.co.codebros.quakesnz.R
 
 /**
  * Created by leandro on 9/08/15.
  */
-class SettingsActivity : DaggerAppCompatActivity() {
+class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
