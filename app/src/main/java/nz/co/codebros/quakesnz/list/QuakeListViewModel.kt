6430f4024438ebe4f8b49e53151cd0f5bfb56c47
Leandro <gringoperalta@gmail.com>
2018-02-13 07:10:54 +1300

    Add disposable state event model.

--- app/src/main/java/nz/co/codebros/quakesnz/list/QuakeListViewModel.kt
+++ app/src/main/java/nz/co/codebros/quakesnz/list/QuakeListViewModel.kt
@@ -3,56 +3,23 @@ package nz.co.codebros.quakesnz.list
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
 import android.content.SharedPreferences
+import android.util.EventLog
 import ar.soflete.cycler.EventModel
-import com.google.android.gms.analytics.HitBuilders
-import com.google.android.gms.analytics.Tracker
 import io.reactivex.disposables.CompositeDisposable
 import nz.co.codebros.quakesnz.error.ErrorEvent
-import nz.co.codebros.quakesnz.interactor.LoadFeaturesInteractor
-import nz.co.codebros.quakesnz.interactor.SelectFeatureInteractor
 import javax.inject.Inject
-import javax.inject.Named
 
 /**
  * Created by Leandro on 27/11/2017.
  */
 class QuakeListViewModel(
         private val sharedPreferences: SharedPreferences,
-        loadFeaturesInteractor: LoadFeaturesInteractor,
-        selectFeatureInteractor: SelectFeatureInteractor,
-        @Named("app") tracker: Tracker
+        val quakeListModel: QuakeListModel,
+        val errorModel: EventModel<ErrorEvent>
 ) : ViewModel() {
-    val quakeListModel = QuakeListModel(QuakeListState(false))
-    val errorModel = EventModel<ErrorEvent>()
 
     private val disposables = CompositeDisposable().apply {
-        add(quakeListModel.publish(quakeListModel.eventObservable
-                .startWith(QuakeListEvent.LoadQuakes())
-                .filter { it is QuakeListEvent.LoadQuakes }
-                .flatMap {
-                    loadFeaturesInteractor.execute()
-                            .map { QuakeListEvent.QuakesLoaded(it.features) as QuakeListEvent }
-                            .onErrorReturn { QuakeListEvent.LoadQuakesError(it) }
-                }))
-
-        add(quakeListModel.eventObservable
-                .filter { it is QuakeListEvent.SelectQuake }
-                .map { (it as QuakeListEvent.SelectQuake).quake }
-                .subscribe { selectFeatureInteractor.execute(it) })
-
-        add(quakeListModel.eventObservable.subscribe {
-            when (it) {
-                QuakeListEvent.RefreshQuakes -> tracker.send(HitBuilders.EventBuilder()
-                        .setCategory("Interactions")
-                        .setAction("Refresh")
-                        .build())
-                is QuakeListEvent.SelectQuake -> tracker.send(HitBuilders.EventBuilder()
-                        .setCategory("Interactions")
-                        .setAction("Select quake")
-                        .build())
-            }
-        })
-
+        add(quakeListModel)
         add(errorModel.publish(quakeListModel.eventObservable
                 .filter { it is QuakeListEvent.LoadQuakesError }
                 .map { it as QuakeListEvent.LoadQuakesError }
@@ -77,11 +44,9 @@ class QuakeListViewModel(
 
     class Factory @Inject constructor(
             private val sharedPreferences: SharedPreferences,
-            private val loadFeaturesInteractor: LoadFeaturesInteractor,
-            private val selectFeatureInteractor: SelectFeatureInteractor,
-            @Named("app") private val tracker: Tracker
+            private val quakeListModel: QuakeListModel
     ) : ViewModelProvider.Factory {
         override fun <T : android.arch.lifecycle.ViewModel> create(modelClass: Class<T>) =
-                QuakeListViewModel(sharedPreferences, loadFeaturesInteractor, selectFeatureInteractor, tracker) as T
+                QuakeListViewModel(sharedPreferences, quakeListModel, EventModel<ErrorEvent>()) as T
     }
 }
